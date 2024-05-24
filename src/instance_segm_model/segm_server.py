09e0ca69697e1a4b95309ce9a263ab74808a1c94
Kurchaviy Vitaliy <vitalykurchaviy@yandex.ru>
2024-05-24 08:59:15 +0300

    change path to model weights in code segm_server.py

--- src/instance_segm_model/segm_server.py
+++ src/instance_segm_model/segm_server.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(BASE_DIR, 'model_data', 'best.pt')
+model_path = os.path.join(BASE_DIR, 'model_weights', 'best.pt')
 model = YOLO(model_path)
 
 @app.route('/predict', methods=['POST'])
